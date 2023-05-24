284ec3f867b3cc865dffb9953e6406d84d90b14b
ZayRex <31854984+ZayRex@users.noreply.github.com>
2023-05-24 19:40:04 +0100

    create new listing layout

---
+++ auctions/models.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
+from django.db import models
+
+class User(AbstractUser):
+    def __str__(self):
+        return self.username
+
+class AuctionListing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField(max_length=500)
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
+    image_url = models.URLField(blank=True, null=True)
+    category = models.CharField(max_length=50, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    creation_time = models.DateField()
+    ending_time = models.DateField()
+
+class Comment(models.Model):
+    listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField(max_length=500)
+    time = models.DateField()
+
+class Bid(models.Model):
+    listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+    bid_amount = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
+    bid_time = models.DateTimeField()
